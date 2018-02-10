777eab59fdba628ac32bee2bbfe7915f701fbe50
Tan Bui <trungtanbui@gmail.com>
2018-02-10 09:51:58 +0200

    Install and Activate events

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,8 +1,9 @@
 // check if `serviceWorker` property is available in navigator (browser object)
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('/sw.js')
+    .register('/sw.js', {scope: '/'})
     .then(function () {
+      //this log could be render before registration finishes (non-blocking)
       console.log('Service Worker registered!');
     });
 }
--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,19 @@
 // Service Worker must be placed in root folder to its scope can access all resources
+// SW only works in HTTPS
+
+/**
+ * `Install` event is called at first registration
+ *  or after sw.js is changed and the page is refreshed.
+ */
+self.addEventListener('install', function(event) {
+  console.log("[SW] Installing Service Worker...", event);
+});
+
+/**
+ * `Activate` event is called after closing all pages which are currently using *old version* of SW
+ */
+self.addEventListener('activate', function(event) {
+  console.log("[SW] Activating Service Worker...", event);
+  return self.clients.claim();
+});
+
