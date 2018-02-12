1a6e7fd70ef10bb84ac6bc004cde71e94df31603
Tan Bui <trungtanbui@gmail.com>
2018-02-12 16:46:03 +0200

    Demo promise reject with catching

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -18,13 +18,19 @@ window.addEventListener('beforeinstallprompt', function(event) {
 
 var promise = new Promise(function(resolve, reject) {
   setTimeout(function() {
-    resolve('The request is resolved after 3s.');
+    //resolve('The request is resolved after 3s.');
+    reject({code: 500, message: 'An error occurs.'});
   }, 3000)
 });
 promise.then(function(data) {
-  return "Processed data: " + data;
-}).then(function(newData) {
-  console.log(newData);
+  return "Processed data: " + data; //this is the first step
+})
+// `.catch` can also be here
+.then(function(newData) {
+  console.log(newData);  //this is the second step
+}).catch(function(err) {
+  //catch any error in any step and preventing executing its later steps
+  console.log(err.code + ": " + err.message);
 });
 
 console.log("This line is executed non-blocking.");
