7b877ab6d1aff5aaaf6ec0b841183628e4217b79
Tan Bui <tan@vaadin.com>
2018-09-09 19:38:20 +0300

    Custom handler for workbox to cach custom requests

--- public/sw-base.js
+++ public/sw-base.js
@@ -1,20 +1,40 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
+importScripts('/src/js/idb.js');
+importScripts('/src/js/utility.js');
 
 workbox.routing.registerRoute(new RegExp('.*(?:gstatic)\.com.*$'), workbox.strategies.staleWhileRevalidate(
     {
         cacheName: 'google-fonts',
         plugins: [
             new workbox.expiration.Plugin({
-              maxAgeSeconds: 7 * 24 * 60 * 60,
-              maxEntries: 5,
+                maxAgeSeconds: 7 * 24 * 60 * 60,
+                maxEntries: 5,
             }),
-          ]
+        ]
     }
 ));
 
 workbox.routing.registerRoute(new RegExp('.*(?:googleapis)\.com.*$'), workbox.strategies.staleWhileRevalidate(
     { cacheName: 'post-resource' }
 ));
+
+workbox.routing.registerRoute('https://pwagram-45678.firebaseio.com/posts.json',
+    (args) => {
+        return fetch(args.event.request)
+            .then(function (res) {
+                var clonedRes = res.clone();
+                clearAllData('posts')
+                    .then(function () {
+                        return clonedRes.json();
+                    })
+                    .then(function (data) {
+                        for (var key in data) {
+                            writeData('posts', data[key]);
+                        }
+                    })
+                return res;
+            })
+    });
 
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute([]);
