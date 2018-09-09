134fcb9097756e9be98787cdf4f5c427b3570c61
Tan Bui <tan@vaadin.com>
2018-09-09 20:18:19 +0300

    Add offline HTML fallback

--- public/sw-base.js
+++ public/sw-base.js
@@ -36,5 +36,33 @@ workbox.routing.registerRoute('https://pwagram-45678.firebaseio.com/posts.json',
             })
     });
 
+workbox.routing.registerRoute((routeData) => {
+    return (routeData.event.request.headers.get('accept').includes('text/html'));
+},
+    (args) => {
+        return caches.match(args.event.request) //auto-match by request object
+            .then(function (response) {
+                if (response) {
+                    return response; // return value from cache, not send out to network
+                } else {
+                    return fetch(args.event.request)    // cache is null, send request to network
+                        .then(function (onlineResponse) {
+                            caches.open('dynamic')
+                                .then(function (cache) {
+                                    // store a clone of Response because Response is only consumed ONCE.
+                                    cache.put(args.event.request.url, onlineResponse.clone());
+                                    return onlineResponse;
+                                })
+                        })
+                        .catch(function (err) {
+                            return caches.match('/offline.html')
+                                .then(function (res) {
+                                    return res;
+                                })
+                        });
+                }
+            })
+    });
+
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute([]);
